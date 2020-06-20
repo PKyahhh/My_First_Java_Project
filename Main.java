e63aed2a330b94188ffb47f2852c5ae5cdc4b4df
PKyahhh <58451812+PKyahhh@users.noreply.github.com>
2020-06-20 11:08:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,48 @@
 package com.pkdev;
 
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Currency;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner temp = new Scanner(System.in);
-        System.out.print("What is the temprature: ");
-        int value = temp.nextInt();
-        if (value >= 30) {
-            System.out.println("It is very hot");
-        } else if (value < 30 && value >= 20){
-            System.out.println("It is nice outside");
-        } else {
-            System.out.println("It is cold outside");
-        }
-        }
+        //find principal value
+        Scanner principal = new Scanner(System.in);
+        System.out.print("Principal: ");
+        double prince = principal.nextDouble();
+
+        //find rate of intrest
+        Scanner intrest = new Scanner(System.in);
+        System.out.print("Annual Intrest Rate: ");
+        double inter = intrest.nextDouble();
+
+        //find how much time there is in years
+        Scanner time = new Scanner(System.in);
+        System.out.print("Amount of time to pay (in years): ");
+        double tim = principal.nextDouble();
+
+        //Calculate mortgage
+        double ti = tim * 12;
+        double intdec = (double)inter / 100 / 12;
+        double intdecuno = intdec + 1;
+        double intpowered = Math.pow(intdecuno,ti);
+        double top = intpowered * intdec;
+        double bottompowered = intpowered;
+        double bottom = bottompowered - 1;
+        double divided = top / bottom;
+        double m = prince * divided;
+
+        //Put value into number format and print
+        NumberFormat curent = NumberFormat.getCurrencyInstance();
+        String finale = curent.format(m);
+        System.out.println(finale);
 
 
+
+        }
     }
 
